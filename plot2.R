0a321ed8fafab7ac4de26e7c0ee4c79f8b665888
Fabricio Nascimento <fabriciosn@gmail.com>
2015-10-11 16:22:13 -0300

    Updating plot 2 generation script

--- plot2.R
+++ plot2.R
@@ -1,18 +1,18 @@
-#Plot1 - Frequency x Global Active Power
+#Plot2 - Frequency x Global Active Power
 
 #Load and Format Data
-#col_types = c('character', 'character', 'numeric','numeric', 'numeric', 'numeric','numeric', 'numeric', 'numeric')
-#data <- read.csv('data/household_power_consumption.txt', sep =";", na.string="?", colClasses = col_types, header= TRUE);
-#data$Date_Time <- strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S")
+col_types = c('character', 'character', 'numeric','numeric', 'numeric', 'numeric','numeric', 'numeric', 'numeric')
+data <- read.csv('data/household_power_consumption.txt', sep =";", na.string="?", colClasses = col_types, header= TRUE);
+data$Date_Time <- strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S")
 
 #Prepare Plot
 from <- as.Date("2007-02-01")
 to <- as.Date("2007-02-02")
 subset_data <- subset(data, as.Date(Date_Time) >= from & as.Date(Date_Time) <= to)
 x_axis <- subset_data$Global_active_power
-y_axis <- weekdays(subset_data$Date)
+y_axis <- subset_data$Date_Time
 
 #Plot to PNG
-#png("plot2.png")
-plot(x_axis, y_axis, type='l', ylab='Global Active Power (kilowatts)', height=480, width=480);
-#dev.off()
+png("plot2.png")
+plot(y_axis, x_axis, type='l', ylab='Global Active Power (kilowatts)', xlab="", height=480, width=480);
+dev.off()
